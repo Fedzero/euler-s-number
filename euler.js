a90c80c96038a068b5056c99d8b2d0202667233e
Fedzero <fedzzero@gmail.com>
2017-01-17 13:00:14 +0100

    Uploaded euler.js

---
+++ euler.js
@@ -0,0 +1,18 @@
+let e = 0;
+let accuracy = 10000;
+
+function factorial(num) {
+  if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+for (let i = 0; i <= accuracy; i++) {
+  e += 1/factorial(i);
+}
+
+return e;
